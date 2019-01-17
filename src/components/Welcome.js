9baec383d122e19938baaa13ba404124690e30c9
Nathan <nathan.fenimore@gmail.com>
2019-01-17 14:59:10 +1000

    navbarbar initial setup with routes

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,35 +1,35 @@
 import React, { Component } from 'react'
-import {api} from '../api/init'
+import { api } from '../api/init'
 
 export default class Welcome extends Component {
 
-constructor(props) {
-  super(props)
+  constructor(props) {
+    super(props)
 
-  this.state = {
-     response: ''
-  }
-}
-
-
-fetchHomepage = async () => {
-    try {
-        const response = await api.get("/")
-        this.setState({response: response.data})
-    } catch(error){
-        console.error("problem retrieving homepage info", error)
+    this.state = {
+      response: ''
     }
-}
+  }
 
-componentDidMount = () => {
-  this.fetchHomepage()
-}
+  fetchHomepage = async () => {
+    try {
+      const response = await api.get("/")
+      console.log(response.data)
+      this.setState({ response: response.data })
+    } catch (error) {
+      console.error("problem retrieving homepage info", error)
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchHomepage()
+  }
 
   render() {
     return (
       <div>
         <h2>Welcome</h2>
-        <p>{  this.state.response  }</p>
+        <p>{this.state.response}</p>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import Nav from './components/Nav';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import NotFound from './components/NotFound';
+import About from './components/About';
+import Contact from './components/Contact';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const routing = (
+    <Router>
+        <div>
+            <Nav />
+            <Switch>
+                <Route exact path='/' component={App} />
+                <Route path='/about' component={About} />
+                <Route path='/contact' component={Contact} />
+                <Route component={NotFound} />
+            </Switch>
+
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
