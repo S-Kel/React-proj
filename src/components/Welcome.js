b777f1bb5ee734b331a8f04f5200854dfd7c0920
Nathan <nathan.fenimore@gmail.com>
2019-01-17 19:26:35 +1000

    cleaned up code in Welome and App components

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,38 +1,38 @@
-import React, { Component } from 'react'
-import { api } from '../api/init'
+import React, { Fragment, Component } from 'react';
+import { api } from '../api/init';
 
 export default class Welcome extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       response: ''
-    }
-  }
+    };
+  };
 
   fetchHomepage = async () => {
     try {
-      const response = await api.get("/")
-      console.log(response.data)
-      this.setState({ response: response.data })
+      const response = await api.get("/");
+      console.log(response.data);
+      this.setState({ response: response.data });
     } catch (error) {
-      console.error("problem retrieving homepage info", error)
-    }
-  }
+      console.error("There was a problem retrieving response from server", error);
+    };
+  };
 
   componentDidMount = () => {
-    this.fetchHomepage()
-  }
+    this.fetchHomepage();
+  };
 
   render() {
     return (
-      <div>
+      <Fragment>
         <h2>Welcome</h2>
         <p>{this.state.response}</p>
-      </div>
-    )
-  }
-}
+      </Fragment>
+    );
+  };
+};
 
 
