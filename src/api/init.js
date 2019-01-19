de3696582c16577a1795d2a8788a6a0caa2530dd
S-Kel <kel.s.stephen@gmail.com>
2019-01-20 01:07:35 +1000

    Initial Commit

--- src/api/init.js
+++ src/api/init.js
@@ -4,7 +4,9 @@ const api = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
       ? "https://protected-scrubland-68099.herokuapp.com"
-      : "http://localhost:3000"
+      : "https://protected-scrubland-68099.herokuapp.com"
+      
+      // : "http://localhost:3000"
 });
 
 export { api };
