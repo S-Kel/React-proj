a35525cf652320240dc7879e2c1937cbc7a9995f
S-Kel <kel.s.stephen@gmail.com>
2019-01-29 00:36:08 +1000

    implemented form slide transition

--- src/components/forms/events/renderEventForm.js
+++ src/components/forms/events/renderEventForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Field, FormSpy } from "react-final-form";
-// import { CSSTransition, TransitionGroup } from "react-transition-group";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 import {
  Grid,
@@ -35,60 +35,28 @@ const CollectUserDetailsForm = (props) => {
  console.log("hasValidationErrors", invalid, pristine, submitting);
  return (
    <div>
-     {/* <CSSTransition key={`ABXC-AX0`} in={true} appear timeout={4500} classNames="fade"> */}
+     <CSSTransition key={'page=0'} in={true} appear timeout={4500} classNames="fade">
      <HostDetailsForm values={values} pristine={pristine} submitting={submitting} invalid={invalid} nextStep={nextStep} prevStep={prevStep} page={page} />
-     {/* </CSSTransition> */}
+     </CSSTransition>
+    
+     <TransitionGroup className='form-container'>
+        {
+            (page === 1 &&
+              <CSSTransition key={page} in={page === 1} appear={page === 1} timeout={500} classNames="slide">
+                <CreateCauseForm values={values} pristine={pristine} submitting={submitting} invalid={invalid} nextStep={nextStep} prevStep={prevStep} page={page} />
+              </CSSTransition>)
+          || (page === 2 &&
+              <CSSTransition key={page} in={page === 2} appear={page === 2} timeout={500} classNames="slide">
+                <CommunityDetailsForm values={values} pristine={pristine} submitting={submitting} invalid={invalid} nextStep={nextStep} prevStep={prevStep} page={page} />
+              </CSSTransition>)
+          || (page === 3 &&
+              <CSSTransition key={page} in={page === 3} appear={page === 3} timeout={500} classNames="slideIn">
+                <ConfirmDetailsForm form='userForm' handleSubmit={handleSubmit} values={values} pristine={pristine} submitting={submitting} invalid={invalid} nextStep={nextStep} prevStep={prevStep} page={page} />
+              </CSSTransition>)
+          || null
 
-     {console.log("Step: ", page)}
-     {console.log("page===1: ", page === 1)}
-     {console.log("page===2: ", page === 2)}
-     {console.log("page===3: ", page === 3)}
-     {
-         (page === 1 && <CreateCauseForm values={values} pristine={pristine} submitting={submitting} invalid={invalid} nextStep={nextStep} prevStep={prevStep} page={page} />)
-      || (page === 2 && <CommunityDetailsForm values={values} pristine={pristine} submitting={submitting} invalid={invalid} nextStep={nextStep} prevStep={prevStep} page={page} />)
-       || (page === 3 && <ConfirmDetailsForm form='userForm' handleSubmit={handleSubmit} values={values} pristine={pristine} submitting={submitting} invalid={invalid} nextStep={nextStep} prevStep={prevStep} page={page} />)
-    }
-     {/* <TransitionGroup className='card-container'> */}
-     {/* {
-     (step === 1 &&
-      <CSSTransition
-       key={step}
-       in={step === 1}
-       appear={step === 1}
-       timeout={500}
-       // mountOnEnter={true}
-       // unmountOnExit={true}
-       classNames="slide">
-       <div className='box-container'>
-        <CreateCauseForm step={step} nextStep={nextStep} prevStep={prevStep} />
-       </div>
-      </CSSTransition>)
-     || (step === 2 &&
-      <CSSTransition
-       key={step}
-       in={step === 2}
-       appear={step === 2}
-       timeout={500}
-       classNames="slide">
-       <div className='box-container'>
-        <CommunityDetailsForm step={step} nextStep={nextStep} prevStep={prevStep} />
-       </div>
-      </CSSTransition>)
-     || (step === 3 &&
-      <CSSTransition
-       key={step}
-       in={step === 3}
-       appear={step === 3}
-       timeout={500}
-       classNames="slide">
-       <div className='box-container'>
-        <ConfirmDetailsForm form="userForm" step={step} nextStep={nextStep} prevStep={prevStep} />
-       </div>
-      </CSSTransition>)
-     || null
-
-    } */}
-     {/* </TransitionGroup> */}
+        }
+     </TransitionGroup>
      <FormSpy subscription={{ values: true }}>
        {({ values }) => <pre>
          <h3 style={{ color: 'red', margin: 20, textAlign: 'center'}}>For Debugging Purposes Only</h3>
