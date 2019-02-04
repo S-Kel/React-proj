87394d7fc57b3477d2fa24c91df6ffe8a3f4ec4f
mitchellheiser <mitchell4436@gmail.com>
2019-02-04 11:58:57 +1000

    cards connected to backend

--- src/components/dashboard/AdminDashboard.js
+++ src/components/dashboard/AdminDashboard.js
@@ -1,57 +1,75 @@
 import React, { Component } from 'react';
 import { Card, Button, Icon, Image, Dropdown, Input  } from 'semantic-ui-react'
+import { api } from '../../api/init';
 
 class AdminDashboard extends Component {
-    state = { data:[
-        {id: 1, name: "Mitchell Heiser", organisation: "company1", createdAt:"1/2/19", shortlisted: true},
-        {id: 2, name: "test2", organisation: "company2", createdAt:"2/2/19", shortlisted: false}
-    ] }
+    constructor(props) {
+        super(props);
+    
+    this.state = { data: [] };
+    }
+    componentDidMount(){
+    api.get('/dashboard?pageNum=x&limit=y').then((response) => {
+      this.setState({ data: response.data.data })
 
+    })
+    }   
+    
+    handleSeeShortlist = event =>{
+        event.preventDefault()
+        api.get('/dashboard/shortlist?pageNum=x&limit=y').then((response) =>{
+            this.setState({data: response.data.data})
+        })
+    }
+
+    handleSeeAll = event => {
+        event.preventDefault()
+        api.get('/dashboard?pageNum=x&limit=y').then((response) => {
+            this.setState({ data: response.data.data })
+    })
+    }
     
 
     
-    render() { 
-        const buildCards = this.state.data.map((Data) =>
+    render() {
         
-        <React.Fragment>
-        <Card.Group centered>
-            <Card color="red" fluid > 
-            <Card.Content color="red"  >
-                    <input  type="hidden" value={Data.id}></input>
-                    <Card.Header key={Data.name} >{Data.name}</Card.Header>
-                    <Card.Meta key={Data.createdAt}>{Data.createdAt}</Card.Meta>
-                    <Card.Description key={Data.organisation}>{Data.organisation}</Card.Description>
-                    <Button animated inverted color="red">
-                    <Button.Content visible>VIEW</Button.Content>
-                    <Button.Content hidden>
-                        <Icon name='arrow right' />
-                    </Button.Content>
-                    </Button>
-                    <div>
-                    {Data.shortlisted ? (<Image floated='right' size='mini' src='/Assets/WBGS-logo.png' />):
-                    (<Image floated='right' size='mini' src='/Assets/WBGS-logo dulled.png' />)}
-                    </div>
-                </Card.Content>
-            </Card>
-        </Card.Group>
-        </React.Fragment>);
-
         return (  
             <React.Fragment>
             <div className="filter">
                 <Dropdown color="red" text='Filter By' icon='filter' floating labeled button className='icon'>
                     <Dropdown.Menu >
                     <Dropdown.Header icon='tags' content='Filter by tag'/>
-                    <Dropdown.Item>Shortlisted</Dropdown.Item>
-                    <Dropdown.Item>See All</Dropdown.Item>
+                    <Dropdown.Item onClick={this.handleSeeShortlist}>Shortlisted</Dropdown.Item>
+                    <Dropdown.Item onClick={this.handleSeeAll}>See All</Dropdown.Item>
                     </Dropdown.Menu>
                 </Dropdown>
              </div>
             <div className="cardContainer">
-             {buildCards}
+             {this.state.data.map((Data) =>
+        
+                <React.Fragment>
+                <Card.Group centered>
+                    <Card color="red" fluid > 
+                    <Card.Content color="red"  >
+                            <input  type="hidden" value={Data._id}></input>
+                            <Card.Header key={Data.first_name} >{Data.host.first_name}</Card.Header>
+                            <Card.Meta key={Data.createdAt}>{Data.createdAt}</Card.Meta>
+                            <Card.Description key={Data.organisation}>{Data.host.organisation}</Card.Description>
+                            <Button animated inverted color="red">
+                            <Button.Content visible>VIEW</Button.Content>
+                            <Button.Content hidden>
+                                <Icon name='arrow right' />
+                            </Button.Content>
+                            </Button>
+                            <div>
+                            {Data.criteria.shortlisted ? (<Image floated='right' size='mini' src='/Assets/WBGS-logo.png' />):
+                            (<Image floated='right' size='mini' src='/Assets/WBGS-logo dulled.png' />)}
+                            </div>
+                        </Card.Content>
+                    </Card>
+                </Card.Group>
+                </React.Fragment>)}
              </div>
-             
-
             </React.Fragment>
         );
     }
